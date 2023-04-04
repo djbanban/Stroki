12d9767b5512e49cf6ad1e0124e9dec7cb726650
TheRomaks <niks_2004@bk.ru>
2023-04-04 21:25:12 +0300

    soob

---
+++ ConsoleStr/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using static Stroki.Parsing;
+
+namespace ParseStroki
+{
+    class ParseStroki
+    {
+        static void Main()
+        {
+            string type = "";
+            Console.ForegroundColor = ConsoleColor.Green;
+            string path = @"D:\progi\billig\stroki\test.txt";
+            string s = " 2.2 ";
+           
+            Console.WriteLine(CheckType(s,type));
+
+        }
+    }
+}
---
+++ Stroki/Parsing.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Channels;
+
+namespace Stroki
+{
+    public class Parsing
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static string CheckType(string s, string type)
+        {
+            s = s.Trim();
+            if (String.IsNullOrEmpty(s))
+                return "пустой файл".ToString();
+            bool isNumeric = int.TryParse(s, out _);
+            bool isDouble = double.TryParse(s, out _);
+            bool isBool = bool.TryParse(s, out _);
+            if (isNumeric)
+                type = "int";
+            else if (isDouble && !s.Contains(' '))
+                type = "double";
+            else if (isBool)
+                type = "bool";
+            else
+                type = "string";
+            return type.ToString();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        public static void Converting(string type) //сами жоский канвертир
+        {
+            if (type == "int")
+                Convert.ToInt32(type).ToString();
+            else if (type == "double")
+                Convert.ToDouble(type).ToString();
+            else if (type == "bool")
+                Convert.ToBoolean(type).ToString();
+            else
+                type.ToString();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        public static void KMPPrefix(string s)
+        {
+
+        }
+        public static void KMPHelping()
+        {
+
+        }
+    }
+}
