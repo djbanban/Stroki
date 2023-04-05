6d1e47d19f8fc8fcae5b50f9990ec2811ea25bf2
Dmitrich7 <pashokkalina@gmail.com>
2023-04-06 00:06:08 +0300

    перввый пунктик

--- ConsoleStr/Program.cs
+++ ConsoleStr/Program.cs
@@ -10,10 +10,20 @@ namespace ParseStroki
     {
         static void Main()
         {
+            int InsertionIndex = 1;
+            int occurances = 10;
             Console.ForegroundColor = ConsoleColor.Green;
-            string s = "2/2";
-            Console.WriteLine(CheckType(s));
-
+            string s = "abcdJIJAabcdJIJAabcd"; // "2/2";
+            //Console.WriteLine(CheckType(s));
+            string subString = "abcd";
+            List<int> subStringIndexList = SlowSubStringSearch(s, subString);
+            //subStringIndexList = SlowSubStringSearch(s, subString);
+            s = SubStringDelete(s, subString, occurances);
+            Console.WriteLine(s);
+            //foreach (int subStringIndex in subStringIndexList)
+            //{
+                //Console.WriteLine(subStringIndex);
+            //}
         }
     }
 }
--- Stroki/Parsing.cs
+++ Stroki/Parsing.cs
@@ -1,9 +1,11 @@
 ﻿using System.Text;
 using System.Threading.Channels;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
 
 namespace Stroki
 {
-    public class Parsing
+    public class Parsing 
     {
 
         const int ap = 46; // ascii point
@@ -126,5 +128,85 @@ namespace Stroki
         {
 
         }
+        //основная kmp функция
+        public static List<int> KMPSearch(string s, string subString)
+        {
+            //лист с индексами начала и конца подстрок(нужен для методов использующиъ поиск подстрок)
+            List<int> subStringIndexList = new List<int>(); 
+
+            return subStringIndexList;
+        }
+        //ещё один велосипед, заменяет подстрочку в строке на новую, учитывая количество вхождений подстроки(указывается пользователем)
+        public static string SubStringReplace(string s,string subString,string newSubString,int occurrences)
+        {
+            List<int> subStringIndexList = KMPSearch(s, subString);
+            if (subStringIndexList.Count/2<occurrences)
+            {
+                occurrences = subStringIndexList.Count/2;
+            }
+            return s;
+        }
+        public static string SubStringDelete(string s, string subString,int occurrences)
+        {
+            
+            List<int> subStringIndexList = SlowSubStringSearch(s, subString);//KMPSearch(s, subString); некич сделай кмп сёрч
+            if (subStringIndexList.Count / 2 < occurrences)
+            {
+                occurrences = subStringIndexList.Count / 2;
+            }
+            for (int j = subStringIndexList.Count; j >= 2; j -= 2)
+            {
+                s = s.Remove(subStringIndexList[j], subStringIndexList[j+1]-subStringIndexList[j] + 1);
+            }
+            return s;
+        }
+        //велосипед(String.Insert())
+        public static string SubStringInsert(string s, string subString, int InsertionIndex)
+        {
+            string newString = "";
+            for (int i = 0;i< InsertionIndex; i++)
+            {
+                newString += s[i];
+            }
+            for (int i = 0; i < subString.Length; i++)
+            {
+                newString += subString[i];
+            }
+            for (int i = InsertionIndex; i < s.Length; i++)
+            {
+                newString += s[i];
+            }
+            return newString;
+        }
+
+        //медленный поиск начальных и конечных индексов подстрок в строке (abccab, ab - 0,1;4,5) и запись их в лист
+        public static List<int> SlowSubStringSearch(string s, string subString) 
+        {
+            List<int> subStringIndexList = new List<int>();
+            int startIndex;
+            int endIndex = 0;
+            bool breakFlag;
+            for (int i = 0;i<s.Length;i++)
+            {
+                startIndex = i;
+                breakFlag = false;
+                for (int j = 0;j<subString.Length;j++)
+                {
+                    if (subString[j] != s[j+i])
+                    {
+                        breakFlag = true;
+                        break;
+                    }
+                    endIndex = j+i;
+                }
+                if (breakFlag == false)
+                {
+                    subStringIndexList.Add(startIndex);
+                    subStringIndexList.Add(endIndex);
+                }
+            }
+            return subStringIndexList;
+        }
+
     }
 }
