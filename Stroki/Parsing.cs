ccf802e7d212da3b26b5c52986baedec8173f850
TheRomaks <niks_2004@bk.ru>
2023-04-05 16:34:50 +0300

    fix

--- Stroki/Parsing.cs
+++ Stroki/Parsing.cs
@@ -5,16 +5,53 @@ namespace Stroki
 {
     public class Parsing
     {
+
         const int ap = 46; // ascii point
         const int ans = 48; // ascii number start
         const int ane = 57; // ascii number end
-        public static string CheckType(string s)
+
+        public static string CheckTypePositive(byte[] asciiBytes, string s, int lenght, string type)
+        {
+            int count = 0;
+            for (int i = 0; i < lenght; i++)
+                if (asciiBytes[i] >= ans && asciiBytes[i] <= ane)
+                    count++;
+            if (count == asciiBytes.Length && asciiBytes[0] != ans) // lenght - 1 тк один знак уходит на "-"
+                type = "int";
+            else if (count == lenght - 2 && asciiBytes[0] != ap && asciiBytes[lenght - 1] != ap && !s.Contains(' '))
+                if (s.Contains('.') || s.Contains(','))
+                    type = "double";
+                else
+                    type = "string";
+            else
+                type = "string";
+            return type.ToString();
+        }
+
+        public static string CheckTypeNegative(byte[] asciiBytes, string s, int lenght, string type)
+        {
+            int count = 0;
+            for (int i = 0; i < lenght; i++)
+                if (asciiBytes[i] >= ans && asciiBytes[i] <= ane)
+                    count++;
+            if (count == asciiBytes.Length - 1 && asciiBytes[0] != ans && asciiBytes[lenght - 1] != 45) // lenght - 1 тк один знак уходит на "-"
+                type = "int";
+            else if (count == lenght - 2 && asciiBytes[0] != ap && asciiBytes[lenght - 1] != ap && !s.Contains(' ') && asciiBytes[0] == 45)
+                if (s.Contains('.') || s.Contains(','))
+                    type = "double";
+                else
+                    type = "string";
+            else
+                type = "string";
+            return type.ToString();
+        }
+
+        public static string CheckType(string s, out string type1)
         {
             string type = "";
             if (String.IsNullOrEmpty(s))
-                return "File is Empty".ToString();
+                return type1 = "File is Empty".ToString();
             s = s.Trim();
-            int count = 0;
             byte[] asciiBytes = Encoding.ASCII.GetBytes(s);
             int lenght = asciiBytes.Length;
             if (s.ToLower() == "false" || s.ToLower() == "true")
@@ -23,42 +60,64 @@ namespace Stroki
                 type = "int";
             else
             {
-                for (int i = 0; i < lenght; i++)
-                    if (asciiBytes[i] >= ans && asciiBytes[i] <= ane)
-                        count++;
-                if (count == asciiBytes.Length && asciiBytes[0] != ans)
-                    type = "int";
-                else if (count == lenght - 1 && asciiBytes[0] != ap && asciiBytes[lenght - 1] != ap && !s.Contains(' '))
-                {
-                    if (s.Contains('.') || s.Contains(','))
-                        type = "double";
-                    else
-                        type = "string";
-                }
+                if (!s.Contains('-'))
+                    type = CheckTypePositive(asciiBytes, s, lenght, type);
                 else
-                    type = "string";
+                    type = CheckTypeNegative(asciiBytes, s, lenght, type);
             }
+            type1 = type;
             return type.ToString();
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="type"></param>
-        public static void Converting(string type) //сами жоский канвертир
+
+        public static int[] CharToInt(char[] letters, int[] c, int n)
         {
-            if (type == "int")
-                Convert.ToInt32(type).ToString();
-            else if (type == "double")
-                Convert.ToDouble(type).ToString();
-            else if (type == "bool")
-                Convert.ToBoolean(type).ToString();
-            else
-                type.ToString();
+            for (int i = 0; i < n; i++)
+                c[i] = (int)Char.GetNumericValue(letters[i]);
+            return c;
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="s"></param>
+
+        public static void CharToDouble(char[] letters, string s) //доделать тк он конвертируеь запятую в -1
+        {
+            s = s.Replace(',', '.');
+            int n = s.Length;
+            int[] c = new int[n];
+            string[] strings = s.Split('.');
+            Console.WriteLine(String.Join(" ", strings));
+            for (int i = 0; i < n; i++)
+            {
+                c[i] = (int)Char.GetNumericValue(letters[i]);
+            }
+            Console.WriteLine(String.Join("", c).Replace('0', ','));
+            return;
+        }
+
+        public static dynamic Converter(string s, string type)
+        {
+            //создание массива char элементов и заполнение его символами из строки
+            char[] letters = s.ToCharArray();
+            int n = letters.GetLength(0);
+            int[] c = new int[n];
+            Console.WriteLine(String.Join(" ", letters));
+            if (type == "bool")
+                return s.ToString();
+            else if (type == "string")
+                return s.ToString();
+            else
+                if (type == "int")
+                CharToInt(letters, c, n);
+            else
+                CharToDouble(letters, s);
+            string result = String.Join("", c);
+            return result.ToString();
+        }
+
+        static void Main()
+        {
+            string? type1 = null;
+            string s = "-1.2";
+            Console.WriteLine($"Проверка типа данных: {CheckType(s, out type1)}");
+            Console.WriteLine($"Конвертированная строка: {Converter(s, type1)}");
+        }
         public static void KMPPrefix(string s)
         {
 
